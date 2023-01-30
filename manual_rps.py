3d5142eb7b98e9e82b4296e2c1672b91b2a2a467
GerryMehay <117312778+GerryMehay@users.noreply.github.com>
2023-01-30 22:03:07 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -11,7 +11,7 @@ def get_user_choice ():
     return user_choice
 
 def get_winner (computer_choice, user_choice):
-  print(f"The computer picked {computer_choice}, you picked {user_choice}")
+    print(f"The computer picked {computer_choice}, you picked {user_choice}")
     if user_choice == computer_choice:
             result = ("It is a tie!")
     elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
