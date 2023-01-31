af185d7df1a3362b17a874e9dc6dc32a19e811c1
GerryMehay <117312778+GerryMehay@users.noreply.github.com>
2023-01-31 10:04:58 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,28 +1,30 @@
+
+
 #%%
 import random 
 
 def get_computer_choice():
-    computer_option = ["rock", "paper", "scissor"]
-    computer_choice = random.choice(computer_option)
+    comp_list = ['rock','paper','scissors']
+    computer_choice = random.choice(comp_list)
     return computer_choice
 
-def get_user_choice ():
+def get_user_choice():
     user_choice = input("Play")
     return user_choice
 
-def get_winner (computer_choice, user_choice):
+
+def get_winner(computer_choice, user_choice):
     print(f"The computer picked {computer_choice}, you picked {user_choice}")
     if user_choice == computer_choice:
             result = ("It is a tie!")
     elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
         result =("You won!")
     else: result = "You lost" 
-    print(result)
-    
+    print(result)    
+
 def play():
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
     get_winner(computer_choice, user_choice)
 
 play()
-
