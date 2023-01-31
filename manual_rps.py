4828b2473192c82f66b59f9e59bbd0d92117b31d
GerryMehay <117312778+GerryMehay@users.noreply.github.com>
2023-01-31 10:50:32 +0000

    Update manual_rps.py

--- manual_rps.py
+++ manual_rps.py
@@ -1,28 +1,33 @@
+
 #%%
-import random 
+
+import random
 
 def get_computer_choice():
-    comp_list = ['rock','paper','scissors']
-    computer_choice = random.choice(comp_list)
-    return computer_choice
+    return random.choice(['Rock', 'Paper', 'Scissors'])
+
 
 def get_user_choice():
-    user_choice = input("Play")
-    return user_choice
-
+    while True:
+        user_choice = input('Please enter your choice (Rock, Paper, Scissors): ')
+        if user_choice in ['Rock', 'Paper', 'Scissors']:
+            return user_choice
+        else:
+            print('Invalid choice. Please try again.')
 
 def get_winner(computer_choice, user_choice):
-    print(f"The computer picked {computer_choice}, you picked {user_choice}")
-    if user_choice == computer_choice:
-            result = ("It is a tie!")
-    elif (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "paper" and user_choice == "scissors") or (computer_choice == "scissors" and user_choice == "rock"):
-        result =("You won!")
-    else: result = "You lost" 
-    print(result)    
+    if computer_choice == user_choice:
+        print('It is a tie!')
+    elif computer_choice == 'Rock' and user_choice == 'Paper':
+        print('You won!')
+    elif computer_choice == 'Paper' and user_choice == 'Scissors':
+        print('You won!')
+    elif computer_choice == 'Scissors' and user_choice == 'Rock':
+        print('You won!')
+    else:
+        print('You lost')
 
 def play():
     computer_choice = get_computer_choice()
     user_choice = get_user_choice()
     get_winner(computer_choice, user_choice)
-
-play()
